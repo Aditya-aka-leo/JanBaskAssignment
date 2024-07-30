3e2bd659032a6dcb7b2e34a35468e3252a0fabf5
Aditya-aka-leo <leohaxter@gmail.com>
2024-07-30 19:40:16 +0530

    Improved Camel Casing

--- q3.py
+++ q3.py
@@ -1,22 +1,23 @@
 def StringMethod( num ):
 
-    NumString = str(num)
+    numString = str(num)
 
-    RevString = NumString[::-1]                     # reversing the string to check for pallindrome
+    revString = numString[::-1]                     # reversing the string to check for pallindrome
 
-    return RevString == NumString
+    return revString == numString
 
 def NumMethod( num ):
 
-    RevNum = 0
+    revNum = 0
+
     temp = num
 
     while(temp != 0) :
-        RevNum = (RevNum * 10) + (temp % 10)        # create a reversed number from original number by extracting from last place
+        revNum = (revNum * 10) + (temp % 10)        # create a reversed number from original number by extracting from last place
 
         temp = temp // 10                           # moving the to the next digit from back
 
-    return RevNum == num 
+    return revNum == num 
 
 num = int(input('Please Enter The Number You Want To Check For Palindrome : '))
 
